bb69d077bb50d71b6231d42026dcc09268670b8c
Xazkiel0 <xazkiel@gmail.com>
2025-08-25 13:07:53 +0700

    feat(seeder): adding seed after reset data

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,25 @@
-import { drizzle } from "drizzle-orm/node-postgres"
-import { Pool } from "pg"
+import { drizzle } from "drizzle-orm/node-postgres";
+import { reset, seed } from "drizzle-seed";
+import { Pool } from "pg";
+
+import "dotenv/config";
+
+import * as schema from "../modules/schema";
 
 const pool = new Pool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "athome",
   database: process.env.DB_NAME || "postgres",
-  ssl: false
-})
+  ssl: false,
+});
 
-export const db = drizzle(pool)
+export const db = drizzle(pool);
+
+async function dbSeeding() {
+  await reset(db, schema);
+  await seed(db, schema, { count: 10 });
+  console.log("Database reset and seeded");
+}
+
+dbSeeding();
---
+++ backend/src/modules/schema.ts
@@ -0,0 +1,3 @@
+export { agama, penduduk, role } from "./penduduk/penduduk.schema";
+export { dusun } from "./dusun/dusun.schema";
+export { kartuKeluarga } from "./kartu_keluarga/kartu_keluarga.schema";
